f4e100c339bfbb74ddedf8305f1bc0fa90263bd5
martasusenkova <susenkova582@gmail.com>
2025-06-09 11:09:45 +0400

    feat: add meme generator with fetch from wholesome memes API

---
+++ script.js
@@ -0,0 +1,25 @@
+const generateMemeBtn = document.querySelector(
+  ".meme__generator .meme__button"
+);
+
+const memeImage = document.querySelector(".meme__generator img");
+
+const memeTitle = document.querySelector(".meme__generator .meme__title");
+
+const memeAuthor = document.querySelector(".meme__generator .meme__author");
+
+const updateDetails = (url, title, author) => {
+  memeImage.setAttribute("src", url);
+  memeAuthor.innerHTML = `Meme by:  ${author}`;
+};
+const generateMeme = () => {
+  fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+      updateDetails(data.url, data.title, data.author);
+    });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme();
